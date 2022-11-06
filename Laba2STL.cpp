d5555f6ebfec21e0779d54e8f651590eb5448f69
Xorek123 <115923870+Xorek123@users.noreply.github.com>
2022-11-06 17:56:01 +0300

    Create Laba2STL.cpp

---
+++ Laba2STL.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void find_prime_number(vector<int>& );
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Введите натуральное число N: ";
+
+    unsigned int number;
+    vector<int> prime_numbers;
+    vector<int>::iterator index;
+
+    while (true) {
+        cin >> number;
+        if (!cin) {
+            cout << "Вероятно вы ввели не число, попробуйте снова.\n ";
+            cin.clear();
+            while (cin.get() != '\n') {}
+        }
+        else{
+
+            for (int i = 2; i != number + 1; ++i) 
+            {
+                prime_numbers.push_back(i);
+            }
+
+            find_prime_number(prime_numbers);
+                
+            for (index = prime_numbers.begin(); index != prime_numbers.end(); ++index) {
+                cout << *index << " ";
+            }
+            }
+            break;
+    }
+}
+
+void find_prime_number(vector<int>& prime_number) {
+    vector<int>::iterator index;
+    vector<int>::iterator second_index = prime_number.begin();
+
+    while (second_index != prime_number.end()) {
+        index = prime_number.begin();
+        while (index != prime_number.end()) {
+            if (*index % *second_index == 0 and *index != *second_index) {
+                index = prime_number.erase(index);
+            }
+            else {
+                ++index;
+            }
+        }
+        ++second_index;
+    }
+}
+
